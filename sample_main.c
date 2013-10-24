f8758726474cfbff22357b3e96ee4bd190fca907
TANABE Makoto <mkttanabe2012@gmail.com>
2013-10-24 17:35:49 +0900

    added following sample functions:
    - sample_removeExifSegment()
    - sample_removeSensitiveData()
    - sample_queryTagExists()
    - sample_updateTagData()
    - sample_saveThumbnail()

--- sample_main.c
+++ sample_main.c
@@ -27,6 +27,13 @@
 
 #include "exif.h"
 
+// sample functions
+int sample_removeExifSegment(const char *srcJpgFileName, const char *outJpgFileName);
+int sample_removeSensitiveData(const char *srcJpgFileName, const char *outJpgFileName);
+int sample_queryTagExists(const char *srcJpgFileName);
+int sample_updateTagData(const char *srcJpgFileName, const char *outJpgFileName);
+int sample_saveThumbnail(const char *srcJpgFileName, const char *outFileName);
+
 // sample
 int main(int ac, char *av[])
 {
@@ -123,9 +130,196 @@ int main(int ac, char *av[])
     // free IFD table array
     freeIfdTableArray(ifdArray);
 
-    // remove the Exif segment from a JPEG file
-    result = removeExifSegmentFromJPEGFile(av[1], "_noexif.jpg");
-    printf("removeExifSegmentFromJPEGFile: result=%d\n", result);
 
+    // sample function A: remove the Exif segment in a JPEG file
+    // result = sample_removeExifSegment(av[1], "removeExif.jpg");
+
+    // sample function B: remove sensitive Exif data in a JPEG file
+    // result = sample_removeSensitiveData(av[1], "removeSensitive.jpg");
+
+    // sample function C: check if "GPSLatitude" tag exists in GPS IFD
+    // result = sample_queryTagExists(av[1]);
+
+    // sample function D: Update the value of "Make" tag in 0th IFD
+    // result = sample_updateTagData(av[1], "updateTag.jpg");
+
+    // sample function E: Write Exif thumbnail data to file
+    // result = sample_saveThumbnail(av[1], "thumbnail.jpg");
+
+    return result;
+}
+
+/**
+ * sample_removeExifSegment()
+ *
+ * remove the Exif segment in a JPEG file
+ *
+ */
+int sample_removeExifSegment(const char *srcJpgFileName, const char *outJpgFileName)
+{
+    int sts = removeExifSegmentFromJPEGFile(srcJpgFileName, outJpgFileName);
+    if (sts <= 0) {
+        printf("removeExifSegmentFromJPEGFile: ret=%d\n", sts);
+    }
+    return sts;
+}
+
+/**
+ * sample_removeSensitiveData()
+ *
+ * remove sensitive Exif data in a JPEG file
+ *
+ */
+int sample_removeSensitiveData(const char *srcJpgFileName, const char *outJpgFileName)
+{
+    int sts, result;
+    void **ifdTableArray = createIfdTableArray(srcJpgFileName, &result);
+
+    if (!ifdTableArray) {
+        printf("createIfdTableArray: ret=%d\n", result);
+        return result;
+    }
+
+    // remove GPS IFD and 1st IFD if exist
+    removeIfdTableFromIfdTableArray(ifdTableArray, IFD_GPS);
+    removeIfdTableFromIfdTableArray(ifdTableArray, IFD_1ST);
+    
+    // remove tags if exist
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_Make);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_Model);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_DateTime);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_ImageDescription);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_Software);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_Artist);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_MakerNote);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_UserComment);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_DateTimeOriginal);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_DateTimeDigitized);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_SubSecTime);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_SubSecTimeOriginal);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_SubSecTimeDigitized);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_ImageUniqueID);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_CameraOwnerName);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_BodySerialNumber);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_LensMake);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_LensModel);
+    removeTagNodeFromIfdTableArray(ifdTableArray, IFD_EXIF, TAG_LensSerialNumber);
+    
+    // update the Exif segment
+    sts = updateExifSegmentInJPEGFile(srcJpgFileName, outJpgFileName, ifdTableArray);
+    if (sts < 0) {
+        printf("updateExifSegmentInJPEGFile: ret=%d\n", sts);
+    }
+    freeIfdTableArray(ifdTableArray);
+    return sts;
+}
+
+/**
+ * sample_queryTagExists()
+ *
+ * check if "GPSLatitude" tag exists in GPS IFD
+ *
+ */
+int sample_queryTagExists(const char *srcJpgFileName)
+{
+    int sts, result;
+    void **ifdTableArray = createIfdTableArray(srcJpgFileName, &result);
+    if (!ifdTableArray) {
+        printf("createIfdTableArray: ret=%d\n", result);
+        return result;
+    }
+
+    sts = queryTagNodeIsExist(ifdTableArray, IFD_GPS, TAG_GPSLatitude);
+    printf("GPSLatitude tag is %s in [%s]\n", (sts) ? "exists" : "not exists", srcJpgFileName);
+
+    freeIfdTableArray(ifdTableArray);
+    return sts;
+}
+
+/**
+ * sample_updateTagData()
+ *
+ * Update the value of "Make" tag in 0th IFD
+ *
+ */
+int sample_updateTagData(const char *srcJpgFileName, const char *outJpgFileName)
+{
+    TagNodeInfo *tag;
+    int sts, result;
+    void **ifdTableArray = createIfdTableArray(srcJpgFileName, &result);
+
+    if (ifdTableArray != NULL) {
+        if (queryTagNodeIsExist(ifdTableArray, IFD_0TH, TAG_Make)) {
+            removeTagNodeFromIfdTableArray(ifdTableArray, IFD_0TH, TAG_Make);
+        }
+    } else { // Exif segment not exists
+        // create new IFD table
+        ifdTableArray = insertIfdTableToIfdTableArray(NULL, IFD_0TH, &result);
+        if (!ifdTableArray) {
+            printf("insertIfdTableToIfdTableArray: ret=%d\n", result);
+            return 0;
+        }
+    }
+    // create a tag info
+    tag = createTagInfo(TAG_Make, TYPE_ASCII, 6, &result);
+    if (!tag) {
+        printf("createTagInfo: ret=%d\n", result);
+        freeIfdTableArray(ifdTableArray);
+        return result;
+    }
+    // set tag data
+    strcpy((char*)tag->byteData, "ABCDE");
+    // insert to IFD table
+    insertTagNodeToIfdTableArray(ifdTableArray, IFD_0TH, tag);
+    freeTagInfo(tag);
+
+    // write file
+    sts = updateExifSegmentInJPEGFile(srcJpgFileName, outJpgFileName, ifdTableArray);
+
+    if (sts < 0) {
+        printf("updateExifSegmentInJPEGFile: ret=%d\n", sts);
+    }
+    freeIfdTableArray(ifdTableArray);
+    return sts;
+}
+
+/**
+ * sample_saveThumbnail()
+ *
+ * Write Exif thumbnail data to file
+ *
+ */
+int sample_saveThumbnail(const char *srcJpgFileName, const char *outFileName)
+{
+    unsigned char *p;
+    unsigned int len;
+    FILE *fp;
+    int result;
+
+    void **ifdTableArray = createIfdTableArray(srcJpgFileName, &result);
+    if (!ifdTableArray) {
+        printf("createIfdTableArray: ret=%d\n", result);
+        return result;
+    }
+
+    // try to get thumbnail data from 1st IFD
+    p = getThumbnailDataOnIfdTableArray(ifdTableArray, &len, &result);
+    if (!p) {
+        printf("getThumbnailDataOnIfdTableArray: ret=%d\n", result);
+        freeIfdTableArray(ifdTableArray);
+        return result;
+    }
+    // save thumbnail
+    fp = fopen(outFileName, "wb");
+    if (!fp) {
+        printf("failed to create [%s]\n", outFileName);
+        freeIfdTableArray(ifdTableArray);
+        return 0;
+    }
+    fwrite(p, 1, len, fp);
+    fclose(fp);
+
+    free(p);
+    freeIfdTableArray(ifdTableArray);
     return 0;
 }
